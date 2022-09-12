2853c036b6623fb365fb8c7a91d2a15da931c017
KapsulonEpitech <theo.bary@epitech.eu>
2022-09-12 16:40:25 +0200

    Fix: correct file paths

--- kepm.py
+++ kepm.py
@@ -3,6 +3,9 @@ import json
 from os import system
 from glob import glob
 import requests
+import git
+
+install_path = "/usr/local/lib/kepm/"
 
 print("Welcome to Kapsulon's Epitech Project Manager")
 print("This program will help you to create a new project or edit an existing one\n")
@@ -66,14 +69,14 @@ def edit_project():
     choice = ask_for_choice("What do you want to do?", ["Edit project name", "Check norm errors", "Quit"])
 
 def check_bubulle() -> bool:
-    if not glob("./Bubulle-Norminette/VERSION"):
+    if not glob(install_path + "Bubulle-Norminette/VERSION"):
         print("Bubulle not found, do you want to install it ?")
         ask = inquirer.prompt([inquirer.Confirm("install", message="Install bubulle ?")])
         if ask["install"]:
             system("git clone https://github.com/aureliancnx/Bubulle-Norminette.git")
             print("Bubulle installed")
             with open("error_catcher_patch", "r") as patch:
-                f = open("./Bubulle-Norminette/bubulle-py/utils/error_handling.py", "w")
+                f = open(install_path + "Bubulle-Norminette/bubulle-py/utils/error_handling.py", "w")
                 content = patch.read()
                 f.write(content)
                 f.close()
@@ -84,16 +87,17 @@ def check_bubulle() -> bool:
             print("Bubulle not installed.")
             return False
     else:
-        version = open("./Bubulle-Norminette/VERSION", "r").read()
+        version = open(install_path + "Bubulle-Norminette/VERSION", "r").read()
         latest = requests.get("https://raw.githubusercontent.com/aureliancnx/Bubulle-Norminette/master/VERSION")
         if version != latest.text:
             print("Bubulle is outdated. Do you want to update it ?")
             ask = inquirer.prompt([inquirer.Confirm("update", message="Update bubulle ?")])
             if ask["update"]:
-                system("cd ./Bubulle-Norminette/ && git pull")
+                g = git.cmd.Git(install_path + "Bubulle-Norminette")
+                g.pull()
                 print("Bubulle updated")
                 with open("error_catcher_patch", "r") as patch:
-                    f = open("Bubulle-Norminette/bubulle-py/utils/error_handling.py", "w")
+                    f = open(install_path + "Bubulle-Norminette/bubulle-py/utils/error_handling.py", "w")
                     content = patch.read()
                     f.write(content)
                     f.close()
@@ -105,7 +109,7 @@ def check_bubulle() -> bool:
                 return False
 
 def check_version():
-    with open("VERSION", "r") as f:
+    with open(install_path + "VERSION", "r") as f:
         version = f.read()
         f.close()
     latest = requests.get("https://raw.githubusercontent.com/Kapsulon/kepm/main/VERSION")
@@ -113,7 +117,8 @@ def check_version():
         print("You are not using the latest version of KEPM. Do you want to update it ?")
         ask = inquirer.prompt([inquirer.Confirm("update", message="Update KEPM ?")])
         if ask["update"]:
-            system("sudo sh -c '$(curl -fsSL https://raw.githubusercontent.com/Kapsulon/kepm/main/install_script.sh)'")
+            g = git.cmd.Git(install_path)
+            g.pull()
             print("KEPM updated")
         else:
             print("KEPM not updated.")
